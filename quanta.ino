1d8e159ddf4f041ff41fffc89fc3402a60656baa
mnnm-macbookair <alberto.harres@gmail.com>
2018-10-02 23:39:48 -0300

    update

--- quanta.ino
+++ quanta.ino
@@ -37,6 +37,9 @@ AudioConnection          patchCord9(mixer1, 0, i2s1, 0);
 AudioControlSGTL5000     sgtl5000_1;     
 
 
+int counter0 = 0;
+int isActivated = 0;
+
 int escala[] = {396, 417, 528, 639, 741, 852, 963 };
 
 //int escala[] = {100, 200, 300, 400, 500, 600, 700};
@@ -50,12 +53,14 @@ int count=1;
 
 int frames = 0;
 
-int sensorPin1 = 0;    // select the input pin for the potentiometer
-int sensorPin2 = 1;    // select the input pin for the potentiometer
+int sensorPin1 = A0;    // select the input pin for the potentiometer
+int sensorPin2 = A1;    // select the input pin for the potentiometer
+int sensorPin3 = A2;  
+int ledPin = 10;  
 
-int sensorValue1 = 0;  // variable to store the value coming from the sensor
-int sensorValue2 = 0;  // variable to store the value coming from the sensor
-
+float sensorValue1 = 0;  // variable to store the value coming from the sensor
+float sensorValue2 = 0;  // variable to store the value coming from the sensor
+float sensorValue3 = 0;
 
 void setup() {
   AudioMemory(18);  
@@ -65,14 +70,17 @@ void setup() {
 
   pinMode(sensorPin1, INPUT);
   pinMode(sensorPin2, INPUT);  
+  pinMode(sensorPin3, INPUT);
 
-  mixer1.gain(0, 0.43);
+  pinMode(ledPin, OUTPUT);
+
+  mixer1.gain(0, -0.43);
   mixer1.gain(1, 0.43);
-  mixer1.gain(2, 0.43);
+  mixer1.gain(2, -0.43);
   mixer1.gain(3, 0.43);
-  mixer2.gain(0, 0.43);
+  mixer2.gain(0, -0.43);
   mixer2.gain(1, 0.43);
-  mixer2.gain(2, 0.43);
+  mixer2.gain(2, -0.43);
    
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.6);
@@ -82,10 +90,12 @@ void setup() {
   for(i =0; i < sizeof(escala)/sizeof(int); i++){
     //waves[i]->begin(amps[i] ,escala[i], WAVEFORM_SINE);   
     waves[i]->begin(0.0,escala[i],WAVEFORM_SINE); 
+    //waves[i]->pulseWidth(1.0);
   }
 
   AudioProcessorUsageMaxReset();
   AudioMemoryUsageMaxReset();
+  
 
   Serial.println("setup done");  
 }
@@ -93,32 +103,59 @@ void setup() {
 unsigned long last_time = millis();
 unsigned long last_time2 = millis();
 void loop() {
+  
   sensorValue1 = analogRead(sensorPin1);
   sensorValue2 = analogRead(sensorPin2);
-
-  float decayRate = (float)map(sensorValue2, 0, 1023, 1, 10000)/10000000;
-   Serial.println(decayRate);
+  sensorValue3 = analogRead(sensorPin3);
+  
+  float decayRate = (map(sensorValue2, 0, 1023, 0.000005, 0.0001));   
   int i;
   for(i =0; i < sizeof(escala)/sizeof(int); i++){               
-    waves[i]->amplitude(amps[i]);      
+    waves[i]->amplitude(amps[i]);   
+   // waves[i]->phase(360);   
     if(pins[i] > 0){
       pins[i] -= 0.01;
       amps[i] += 0.003;
     } else {
       if(amps[i] > 0.0){        
         amps[i] -= decayRate;
-      }        
+      }
     }
   }   
   if(millis() - last_time2 >= 10){
-    Serial.println("sensor1  : ");
-    Serial.println(sensorValue1);
-    Serial.println("sensor2  : ");
-    Serial.println(sensorValue2);
+    frames++;
+    //Serial.println("sensor1  : ");
+    //Serial.println(sensorValue1);
+    //Serial.println("sensor2  : ");
+    //Serial.println(sensorValue2);
+    
+    if(sensorValue3 > 1024/2){
+      if(counter0 < 100) {
+        counter0++;
+        if(isActivated == 0){
+          isActivated = 1; 
+          //quanta(1);
+          //Serial.println("ATIVADO");
+        }
+      }   
+      //Serial.println("HIGH ");
+    } else {      
+      if(counter0 > 0) {
+        counter0--;
+      }      
+      //Serial.println("LOW ");      
+    }
+    
+    if(counter0 == 0 && isActivated == 1){
+      isActivated = 0;
+    }
+
+    //if(frames % 10 == 0) Serial.println(counter0);
+    
     last_time2 = millis();
   }  
    
-  if(millis() - last_time >= 50 + sensorValue1*2 + random(sensorValue1)){
+  if(millis() - last_time >= 50 + random(sensorValue1*10000)){
     quanta(random(7));
     last_time = millis();
   }  
@@ -131,6 +168,11 @@ void quanta(int index){
   if(pins[i] < 0.2){
     pins[i] = 1;
   }
-  Serial.println(index);
-  Serial.println(amps[index]);
+  Serial.print(millis());
+  Serial.print(" : ");   
+  Serial.println(index);     
+  digitalWrite(ledPin, HIGH);
+  delay(50);
+  digitalWrite(ledPin, LOW);
+  //Serial.println(amps[index]);
 }
